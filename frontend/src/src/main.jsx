49a6e12fb3f96d59c10898c59482f6a4fbedcb1a
Israelobuk <israelobuk@users.noreply.github.com>
2026-04-20 19:46:46 -0400

    Clean merged duplicate frontend/backend files from remote history

--- frontend/src/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/redesign.css";
-import "./styles/dashboard.css";
-import "./styles/animations.css";
-import "./styles/home.css";
-
-createRoot(document.getElementById("root")).render(<App />);
